b90160b6b123d6ee1194c4c32c94a81976bf0c6e
Ahmed Mohamed <ahmedronaldooo68@gmail.com>
2025-12-20 13:51:16 +0200

    Hi
    Implemented hill climbing algorithm for N-Queens problem, replacing DFS solution.

--- Hill climbing/coode.py
+++ Hill climbing/coode.py
@@ -1,30 +1,50 @@
-# Simple DFS solution for N-Queens
-# very basic and easy to understand
+import random
 
-def safe(board, row, col):
-    for c in range(col):
-        if board[c] == row:
-            return False
-        if abs(board[c] - row) == abs(c - col):
-            return False
-    return True
+def count_attacks(board):
+    attacks = 0
+    n = len(board)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if board[i] == board[j]:
+                attacks += 1
+            if abs(board[i] - board[j]) == abs(i - j):
+                attacks += 1
+    return attacks
 
-def dfs(board, col, n):
-    if col == n:
-        return True
+def hill_climbing(n):
+    board = []
+    for i in range(n):
+        board.append(random.randint(0, n - 1))
 
-    for r in range(n):
-        if safe(board, r, col):
-            board[col] = r
-            if dfs(board, col + 1, n):
-                return True
-            board[col] = -1
-    return False
+    while True:
+        current_attacks = count_attacks(board)
+        if current_attacks == 0:
+            return board
 
-def solve(n):
-    board = [-1] * n
-    dfs(board, 0, n)
-    return board
+        best_board = board
+        best_attacks = current_attacks
 
-sol = solve(8)
-print("Solution:", sol)
+        for row in range(n):
+            for col in range(n):
+                if col != board[row]:
+                    temp = board.copy()
+                    temp[row] = col
+                    attacks = count_attacks(temp)
+
+                    if attacks < best_attacks:
+                        best_attacks = attacks
+                        best_board = temp
+
+        if best_attacks == current_attacks:
+            return None
+
+        board = best_board
+
+
+n = 8
+result = hill_climbing(n)
+
+if result is None:
+    print("No solution found")
+else:
+    print("Solution:", result)
